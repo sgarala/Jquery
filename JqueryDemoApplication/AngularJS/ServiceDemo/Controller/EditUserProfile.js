a0c55f83a00faa2658520eb5d99cdd2083a8fde1
sgarala <s.r.garala@gmail.com>
2014-11-01 16:08:03 +0530

    Added Angular Js files

---
+++ JqueryDemoApplication/AngularJS/ServiceDemo/Controller/EditUserProfile.js
@@ -0,0 +1,9 @@
+﻿
+var demoApp = angular.module('demoApp', []);
+
+demoApp.controller('EditUserProfile', function ($scope, gravatarUrlBuilder) {
+    $scope.gravatar = function (Email) {
+        return gravatarUrlBuilder.buildGravatarUrl(Email);
+    };
+
+});
---
+++ JqueryDemoApplication/AngularJS/ServiceDemo/Service/EditUserProfileService.js
@@ -0,0 +1,15 @@
+﻿demoApp.factory('gravatarUrlBuilder', function() {
+    return {
+        buildGravatarUrl: function (Email) {
+            var defaultGravetar = 'http://www.gravatar.com/avatar/000?s=200';
+            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (!re.test(Email))
+                return defaultGravetar;
+            //Email = trim(Email); // "MyEmailAddress@example.com"
+            //Email = converters.toLowerCase(Email); // "myemailaddress@example.com"
+            var mD5 = calcMD5(Email);
+            defaultGravetar = 'http://www.gravatar.com/avatar/' + mD5 + '.jpg?s=200&r=g';
+            return defaultGravetar;
+        }
+    }
+});
